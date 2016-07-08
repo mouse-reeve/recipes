bbb65fe46ad00d7791169e6c0459ad98e5e87011
Mouse Reeve <mousereeve@riseup.net>
2016-07-08 15:37:26 -0700

    VTC generation for the VTC repo

--- addToVTC.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-file=$1
-echo $file
-html_file=$( echo $file | sed -e 's/json\/\(.*\).json/\1.html/' )
-python generateHTML.py $file > ../veganteaparty.club/teaparty/recipe/$html_file
