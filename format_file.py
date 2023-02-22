68136e57f034a967cb38b9dc6d7e9e457c704f8d
Mouse Reeve <mousereeve@riseup.net>
2023-02-22 12:29:47 -0800

    Updates python file to do more

--- format_file.py
+++ format_file.py
@@ -1,11 +1,36 @@
 """ Creates a recipe markdown file """
+import argparse
 import json
-import sys
+import os
+import re
 
 from jinja2 import Template
 
 if __name__ == "__main__":
-    recipe = json.load(open(sys.argv[1]))
-    output_format = sys.argv[2] if len(sys.argv) > 2 else "md"
-    template = Template(open(f"template.{output_format}").read())
-    print(template.render(recipe=recipe))
+    parser = argparse.ArgumentParser(
+        prog="Format Recipe File",
+        description="Creates a recipe file in a given format",
+    )
+
+    parser.add_argument("filename", action="store", type=str)
+    parser.add_argument(
+        "-f", "--format", choices=["md", "html"], required=True, type=str
+    )
+
+    args = parser.parse_args()
+    filename = args.filename
+    file_format = args.format
+
+    with open(filename, "r", encoding="utf-8") as json_file:
+        recipe_data = json.load(json_file)
+
+    with open(f"template.{file_format}", "r", encoding="utf-8") as json_file:
+        template = Template(json_file.read())
+
+    dir_name = {"md": "markdown", "html": "html"}
+    output_dir = re.sub(r"^json", dir_name[file_format], os.path.dirname(filename))
+    output_name = re.sub(r"json$", file_format, os.path.basename(filename))
+    output_filename = os.path.join(output_dir, output_name)
+
+    with open(output_filename, "w", encoding="utf-8") as output_file:
+        output_file.write(template.render(recipe=recipe_data))
