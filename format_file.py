119a377180e6617400c913a14407e79a26bc07fb
Mouse Reeve <mousereeve@riseup.net>
2023-02-25 10:55:22 -0800

    Extract quantities

--- format_file.py
+++ format_file.py
@@ -1,5 +1,6 @@
 """ Creates a recipe markdown file """
 import argparse
+import fractions
 import glob
 import json
 import os
@@ -15,7 +16,40 @@ def ingredient_display(value):
     return re.sub(r"[{}]", "", value)
 
 
+def get_computable_quantity(quantity):
+    """1/2 to 0.5, for example"""
+    try:
+        return float(quantity)
+    except ValueError:
+        pass
+    try:
+        fraction = fractions.Fraction(quantity)
+    except ValueError:
+        return 0
+    return fraction.numerator / fraction.denominator
+
+
+def ingredient_data(value):
+    """Extract ingredient data"""
+    # find teh first set of numbers
+    result = re.findall(r"([^\d]*)([\d\/.]+)([^\d]*)", value)
+    if not result:
+        yield [{"text": ingredient_display(value)}]
+
+    for group in result:
+        yield [
+            {"type": "text", "text": ingredient_display(group[0])},
+            {
+                "type": "quantity",
+                "text": group[1],
+                "value": get_computable_quantity(group[1]),
+            },
+            {"type": "text", "text": ingredient_display(group[2])},
+        ]
+
+
 jinja2.filters.FILTERS["ingredient_display"] = ingredient_display
+jinja2.filters.FILTERS["ingredient_data"] = ingredient_data
 
 
 def get_output_path(input_path, output_format):
@@ -46,7 +80,9 @@ def write_file(file_index, input_path, output_path, output_format):
         recipe_data = json.load(json_file)
 
     # Compile the Jinja template
-    with open(f"templates/template.{output_format}", "r", encoding="utf-8") as json_file:
+    with open(
+        f"templates/template.{output_format}", "r", encoding="utf-8"
+    ) as json_file:
         template = jinja2.Template(json_file.read())
 
     # Write the output file
