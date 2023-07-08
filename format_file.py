ae2b457ed0035884f6bae21985e04b3361de08a6
Mouse Reeve <mousereeve@riseup.net>
2023-07-08 07:50:18 -0700

    Adds tag index to html

--- format_file.py
+++ format_file.py
@@ -27,6 +27,13 @@ def get_output_path(input_path, output_format):
     return os.path.join(output_dir, output_name)
 
 
+def get_link_path(input_path, output_format):
+    """Re-compose the url for the output"""
+    output_dir = re.sub(r"^json/", "", os.path.dirname(input_path))
+    output_name = re.sub(r"json$", output_format, os.path.basename(input_path))
+    return os.path.join(output_dir, output_name)
+
+
 def get_file_index(output_format):
     """Get a list of available files in a usable form"""
     file_list = glob.glob(FILES_PATH)
@@ -36,9 +43,8 @@ def get_file_index(output_format):
         with open(item, "r", encoding="utf-8") as recipe_file:
             json_data = json.load(recipe_file)
             title = json_data["title"]
-        link_path = "/".join(item.split("/")[1:])
-        file_link = re.sub(r"json$", output_format, link_path)
-        index[subdir][file_link] = title
+        link_path = get_link_path(item, output_format)
+        index[subdir][link_path] = title
     return index
 
 
@@ -60,8 +66,29 @@ def write_file(file_index, input_path, output_path, output_format):
         )
 
 
+def get_index_data(output_format):
+    """collate by tags and ingredients"""
+    file_list = glob.glob(FILES_PATH)
+    tags = defaultdict(lambda: {})
+    for item in file_list:
+        with open(item, "r", encoding="utf-8") as recipe_file:
+            json_data = json.load(recipe_file)
+            title = json_data["title"]
+            recipe_tags = json_data["tags"]
+        link_path = get_link_path(item, output_format)
+
+        for tag in recipe_tags:
+            if not tag:
+                continue
+            tags[tag][link_path] = title
+    return {"tags": tags}
+
+
 def write_index_file(file_index, output_format):
     """Create the file index"""
+    # get special index data
+    data = get_index_data(output_format)
+
     output_path = get_output_path("json/index.json", output_format)
     # Compile the Jinja template
     template = ENV.get_template(f"{output_format}/index.{output_format}")
@@ -70,7 +97,9 @@ def write_index_file(file_index, output_format):
     # Write the output file
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     with open(output_path, "w", encoding="utf-8") as output_file:
-        output_file.write(template.render(current_dir=subdir, index=file_index))
+        output_file.write(
+            template.render(indices=data, current_dir=subdir, index=file_index)
+        )
 
 
 if __name__ == "__main__":
